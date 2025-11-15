b53c39d45d80d1f48bc54b387cc181b1d9f9738a
psociksec <81356797+psocik@users.noreply.github.com>
2025-11-15 18:56:23 +0100

    reconfig

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "www.cybernetyka.org",
+    pageTitle: "ict.cybernetyka.org",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "www.cybernetyka.org",
+    baseUrl: "ict.cybernetyka.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
